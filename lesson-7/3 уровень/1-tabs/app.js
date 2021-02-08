a81b99523d3b958eaddab0bf24521dfece8cb358
PankratovaE <pankratova_e@mail.ru>
2021-02-08 12:01:49 +0300

    Домашняя работа к уроку 7

---
+++ lesson-7/3 уровень/1-tabs/app.js
@@ -0,0 +1,62 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+let divEl = document.querySelector('.text');
+let links = document.querySelectorAll('.nav-link');
+
+links.forEach(function(element) {
+    element.addEventListener('click', clickHandler);
+});
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+function clickHandler(event) {
+    // здесь вызывайте changeText и changeActiveClass, и передавайте
+    // им объект события.
+    changeActiveClass(event.target);
+    changeText(event.target);
+    //console.log(event.target);
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(link) {
+    //console.log(link);
+    links.forEach(function(element) {
+        element.classList.remove('active');
+    });
+    link.classList.add('active');
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(link) {
+    //console.log(link);
+    if (link.textContent == "Link 1") {
+        divEl.innerHTML = texts.text1;
+    } else if (link.textContent == 'Link 2') {
+        divEl.innerHTML = texts.text2;
+    } else {
+        divEl.innerHTML = texts.text3;
+    }
+    console.log(links);
+}
